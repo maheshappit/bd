81ccf9dfea2cb274f2440e1959e0d7504b5879d0
maheshappit <mahesh.merugu@appitsoftware.com>
2023-11-02 17:10:49 +0530

    auth guards added

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/login',[AdminController::class,'login_form'])->name('login.form');
+Route::post('login-functionality',[AdminController::class,'login_functionality'])->name('login.functionality');
+Route::group(['middleware'=>'admin'],function(){
+    Route::get('logout',[AdminController::class,'logout'])->name('logout');
+    Route::get('dashboard',[AdminController::class,'dashboard'])->name('dashboard');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
